3fd1c8748c05d7fc0f3c9f64c274e2f93dd956f7
Andre_Rodrigues_JR <lariotyler6@gmail.com>
2025-09-30 10:24:34 +0200

    feat: update

--- lib/experiencia-actions.ts
+++ lib/experiencia-actions.ts
@@ -88,7 +88,7 @@ export async function updateExperience(id: string, data: Experience): Promise<Ap
     if (!id) throw new Error("ID é obrigatório para atualização");
     validateExperienceData(data);
     
-    const res = await fetch(`${API_URL}/update-experiencia/${id}`, {
+    const res = await fetch(`${API_URL}/actualizar-experiencia-profissional/${id}`, {
       method: "PUT",
       headers: await authHeaders(),
       body: JSON.stringify(data),
