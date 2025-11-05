27cedd9fd65e2b10af4b9b59ebaf2453d7238994
Andre-Roddrigues <lariotyler6@gmail.com>
2025-11-05 16:26:01 +0200

    feat: nextconfig

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
- 
-  // ⚠️ serverActions com bodySizeLimit vai direto em nextConfig
-  serverActions: {
-    bodySizeLimit: "10mb",
+  // ✅ Colocar dentro de experimental para evitar erro
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "10mb",
+    },
   },
+
+  // ✅ Suporte para importar SVG como componente React
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
+      issuer: /\.[jt]sx?$/, // evita conflito com arquivos de imagem
       use: ["@svgr/webpack"],
     });
     return config;
